ac391e5558963efe1489c76f327df3d5f40f4542
sehun-kim <rogarithm@gmail.com>
2024-02-26 00:53:19 +0900

    문제 원인을 알았다

--- lib/runner.rb
+++ lib/runner.rb
@@ -6,7 +6,8 @@ class Runner
   end
 
   def run
+    #@im.make_partial_path_list "../data/posted_at_raw"
     #@im.make_url_list4index_page "../data/posted_at"
-    @im.make_posts_info "../data/index_page_urls"
+    @im.make_posts_info "../data/index_page_urls_first_15"
   end
 end
